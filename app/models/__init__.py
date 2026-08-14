00a12e30ccd11eaf7a891c8986f909782fe06ef0
Rahul Kumar Jha <jarahul989@gmail.com>
2026-08-14 20:28:23 +0530

    feat: register APXOR SQLAlchemy models

---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from app.models.guild import Guild
+from app.models.security import SecurityChannel, SecurityConfig, SecurityRole
+
+__all__ = ["Guild", "SecurityConfig", "SecurityRole", "SecurityChannel"]
